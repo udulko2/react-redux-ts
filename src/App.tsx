e13c26f9e131b8f6849a624fbfb0933f5cce9702
udu-local <106012463+udulko2@users.noreply.github.com>
2023-07-05 15:12:40 +0300

    project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import TodoList from './components/TodoList';
+import UserList from './components/UserList';
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <UserList />
+      <hr />
+      <TodoList />
+    </div>
   );
 }
 
